01675d8cb7285c16820a90241da49eeb5a53c7e6
Joshua Peck <peckjj@dukes.jmu.edu>
2023-06-04 21:43:21 -0400

    Initial Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FirstPageComponent } from './first-page/first-page.component';
+import { SecondPageComponent } from './second-page/second-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "first", component: FirstPageComponent},
+  {path: "second", component: SecondPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
